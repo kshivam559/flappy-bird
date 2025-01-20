af64c876c22408e98cc7aa03a29bcf261041479c
Shivam Kumar <kshivam559@gmail.com>
2025-01-20 13:51:40 +0530

    changes

---
+++ pwabuilder-adv-sw.js
@@ -0,0 +1,3 @@
+
+  import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
+  precacheAndRoute([{"revision":"77486bafa2c79fe16815ddeb55c66712","url":"game.js"},{"revision":"2579f9ad9a381261725e4660d61a9ee8","url":"index.html"},{"revision":"af87b514212ff3190fb9d78797ce0515","url":"style.css"}]);
